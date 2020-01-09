0cc8c589e0d57cad7b8edb8e5454df898c44c9b6
Heidi Gally <heidigally@gmail.com>
2020-01-09 14:59:45 -0600

    collection-practice-1 finished

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(array)
+    array.sort
+end
 
+def sort_array_desc(array)
+    array.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+    array.sort_by(&:length)
+end
+
+def swap_elements(array)
+    array.insert(1, array.delete_at(2))
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(string_array)
+    new_array = []
+    string_array.each do |string|
+      new_array << string.sub(string[2], '$')
+    end
+    new_array
+  end
+
+def find_a(array)
+  array.select do |word|
+    word.start_with? ("a")
+  end
+end
+
+def sum_array(array)
+  array.sum
+end
+
+def add_s(array)
+  array.each_with_index.collect do |string, index|
+    if index == 1
+      string
+    else
+    string << 's'
+    end
+  end
+end
